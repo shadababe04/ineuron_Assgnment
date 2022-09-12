2b69664462d28f621ef8373a9c28c68620eaff0a
shadababe04 <shadababe04@gmail.com>
2022-09-12 22:38:56 +0530

    3 ques complete

---
+++ Assignment_1/Ques_1/main.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("Hello Student\n");
+    return 0;
+}
---
+++ Assignment_1/Ques_2/main.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("Hello \nStudent\n");
+    return 0;
+}
