08ff76df6a4342655575e3e15771f66772e46000
shadababe04 <shadababe04@gmail.com>
2022-09-13 15:05:19 +0530

    Assignment 1 complete

---
+++ Assignment_1/Ques_11/main.c
@@ -0,0 +1,18 @@
+/**
+ * @file    main.c
+ * @author  shadab
+ * @brief   WAP to take time as an input in below given format and convert the time format and 
+ *          display the result as given below. 
+ *          User Input date format – “HH:MM” 
+ *          Output format – “HH hour and MM Minute” 
+ *          Example – 
+ *          “11:25” converted to “11 Hour and 25 Minute”
+ * @version 0.1
+ * @date 2022-09-13
+ * 
+ */
+#include<stdio.h>
+int main()
+{
+    return 0;
+}
---
+++ Assignment_1/Ques_12/main.c
@@ -0,0 +1,18 @@
+/**
+ * @file main.c
+ * @author shadab
+ * @brief 
+ * @version 0.1
+ * @date 2022-09-13
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include<stdio.h>
+int main() 
+{   
+ int x = printf("ineuron"); 
+ printf("%d",x); 
+  return 0; 
+} 
+ 
